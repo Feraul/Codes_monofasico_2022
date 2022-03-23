d78fe564d0ced2738616507b4d0e6e4c52697a3b
Fernando Contreras <ferlicapac@gmail.com>
2022-03-23 17:02:04 -0300

    organized

--- fullpicard.m
+++ fullpicard.m
@@ -2,34 +2,29 @@ function [p_new,tabletol,step,ciclos]=fullpicard(M_old,RHS_old,nitpicard,tolpica
     parameter,metodoP,w,s,nflagface,fonte,p_old,gamma,nflagno,benchmark,...
     weightDMP,auxface,wells,mobility,Hesq, Kde, Kn, Kt, Ded,calnormface,gravresult)
 ciclos=1;
-%% calculo do residuo Inicial
+% calculo do residuo Inicial
 R0=norm(M_old*p_old-RHS_old);
 
-%% inicializando dados para iteração Picard
+% inicializando dados para iteração Picard
 step=0;
 er=1;
 contador=0;
 while (tolpicard<er || tolpicard==er) && (step<nitpicard)
     %% atualiza iterações
     step=step+1
+    %utilizando o precondicionado
+    [L,U] = ilu(M_old,struct('type','ilutp','droptol',1e-6));
+    % calculo do novo campo de pressao
+    [p_new]=gmres(M_old,RHS_old,1,1e-9,1000,L,U,p_old);
+    % plotagem no visit
     
-%    p_new=M_old\RHS_old;  % inversão sem pivotamento
-%     if rcond(full(M_old))<1e-5
-        [L,U] = ilu(M_old,struct('type','ilutp','droptol',1e-6));
-%        [p_new,fl1,rr1,it1,rv1]=bicgstab(M_old,RHS_old,10^-12,150);%,100,L,U,p_old);
-%     else
-%        [p_new,fl1,rr1,it1,rv1]=bicgstab(M_old,RHS_old,min(10^-5,10^-1*er),1000);
-%    end
-    [p_new]=gmres(M_old,RHS_old,10,1e-9,1000,L,U);
-    %% plotagem no visit
-         S=ones(size(p_new,1),1);
-         postprocessor(p_new,step)
-%         p_max=max(p_new)
-%         p_min=min(p_new)
-    %% Interpolação das pressões na arestas (faces)
+    postprocessor(p_new,step)
+    %         p_max=max(p_new)
+    %         p_min=min(p_new)
+    % Interpolação das pressões na arestas (faces)
     [pinterp_new]=pressureinterp(p_new,nflagface,nflagno,w,s,metodoP,parameter,weightDMP);
     
-    %% Calculo da matriz global
+    % Calculo da matriz global
     [M_new,RHS_new]=globalmatrix(p_new,pinterp_new,gamma,nflagface,nflagno...
         ,parameter,kmap,fonte,metodoP,w,s,benchmark,weightDMP,auxface,...
         wells,mobility,Hesq, Kde, Kn, Kt, Ded,calnormface,gravresult);
@@ -42,13 +37,13 @@ while (tolpicard<er || tolpicard==er) && (step<nitpicard)
     else
         er = 0.0; %exact
     end
-
+    
     M_old=M_new;
     RHS_old=RHS_new;
     p_old=p_new;
-    %if rem(step,5)==0 || step==1
+    
     tabletol(contador+1,1:2)=[contador, er];
     contador=contador+1;
-    %end
+    
 end
 end
