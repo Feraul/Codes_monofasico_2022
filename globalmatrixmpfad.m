c2060a92b633d0dccf21aebb7b0f4a02b2a20694
Fernando Contreras <ferlicapac@gmail.com>
2022-03-30 12:48:34 -0300

    manipulacoes da matriz incluindo termo gravitacional

--- globalmatrixmpfad.m
+++ globalmatrixmpfad.m
@@ -1,6 +1,6 @@
 function [ M, I ] = globalmatrixmpfad( w,s, Kde, Ded, Kn, Kt, nflag, ...
-                                          Hesq,wells,mobility,fonte,...
-                                          gravresult,gravrate,pinterp)
+    Hesq,wells,mobility,fonte,...
+    gravresult,gravrate,pinterp)
 
 global coord elem esurn1 esurn2  bedge inedge  centelem elemarea bcflag
 
@@ -10,44 +10,44 @@ global coord elem esurn1 esurn2  bedge inedge  centelem elemarea bcflag
 M=sparse(size(elem,1),size(elem,1)); %Prealocação de M.
 I=sparse(size(elem,1),1);
 % fonte
-I=I+fonte+gravresult;
+I=I+fonte;%-gravresult;
 % contribuição dos poços
 
-    for ifacont=1:size(bedge,1)
+for ifacont=1:size(bedge,1)
+    
+    v0=coord(bedge(ifacont,2),:)-coord(bedge(ifacont,1),:); %fase.
+    v1=centelem(bedge(ifacont,3),:)-coord(bedge(ifacont,1),:);
+    v2=centelem(bedge(ifacont,3),:)-coord(bedge(ifacont,2),:);
+    normcont=norm(v0);
+    % Tratamento do nó nos vértices 2 e 4%
+    
+    if bedge(ifacont,5)<200
+        c1=nflag(bedge(ifacont,1),2);
+        c2=nflag(bedge(ifacont,2),2);
         
-        v0=coord(bedge(ifacont,2),:)-coord(bedge(ifacont,1),:); %fase.
-        v1=centelem(bedge(ifacont,3),:)-coord(bedge(ifacont,1),:);
-        v2=centelem(bedge(ifacont,3),:)-coord(bedge(ifacont,2),:);
-        normcont=norm(v0);
-        % Tratamento do nó nos vértices 2 e 4%
+        A=-Kn(ifacont)/(Hesq(ifacont)*norm(v0));
         
-        if bedge(ifacont,5)<200
-            c1=nflag(bedge(ifacont,1),2);
-            c2=nflag(bedge(ifacont,2),2);
-            
-            A=-Kn(ifacont)/(Hesq(ifacont)*norm(v0));
-            
-            %Preenchimento
-            
-            M(bedge(ifacont,3),bedge(ifacont,3))=M(bedge(ifacont,3),bedge(ifacont,3))-A*(norm(v0)^2);
-            
-            I(bedge(ifacont,3))=I(bedge(ifacont,3))-(dot(v2,-v0)*c1+dot ...
-                (v1,v0)*c2)*A+(c2-c1)*Kt(ifacont);
-            
-        else
-            % contorno de Neumann
-            x=bcflag(:,1)==bedge(ifacont,5);
-            r=find(x==1);
-            I(bedge(ifacont,3))=I(bedge(ifacont,3)) -normcont*bcflag(r,2);
-        end
+        %Preenchimento
         
-       % I(bedge(ifacont,3))=I(bedge(ifacont,3))+gravrate(ifacont,1); 
+        M(bedge(ifacont,3),bedge(ifacont,3))=M(bedge(ifacont,3),bedge(ifacont,3))-A*(norm(v0)^2);
+        
+        I(bedge(ifacont,3))=I(bedge(ifacont,3))-(dot(v2,-v0)*c1+dot ...
+            (v1,v0)*c2)*A+(c2-c1)*Kt(ifacont)-normcont*A*gravrate(ifacont,1);
+      
+    else
+        % contorno de Neumann
+        x=bcflag(:,1)==bedge(ifacont,5);
+        r=find(x==1);
+        I(bedge(ifacont,3))=I(bedge(ifacont,3)) -normcont*bcflag(r,2);
     end
+    
+   
+end
 
 
 % contribuição nas faces internas
 for iface=1:size(inedge,1)
-    
+    norma=norm(coord(inedge(iface,1),:)-coord(inedge(iface,2),:));
     %Contabiliza as contribuições do fluxo numa aresta para os elementos %
     %a direita e a esquerda dela.                                        %
     
@@ -106,8 +106,8 @@ for iface=1:size(inedge,1)
             M(inedge(iface,4), esurn1(post_cont))=M(inedge(iface,4),esurn1(post_cont)) + Kde(iface)*Ded(iface)*w(post_cont);
         end
     end
-%     I(inedge(iface,3))=I(inedge(iface,3))+gravrate(iface+size(bedge,1),1);
-%     I(inedge(iface,4))=I(inedge(iface,4))-gravrate(iface+size(bedge,1),1);
+     I(inedge(iface,3))=I(inedge(iface,3))-norma*Kde(iface)*gravrate(iface+size(bedge,1),1);
+     I(inedge(iface,4))=I(inedge(iface,4))+norma*Kde(iface)*gravrate(iface+size(bedge,1),1);
 end
 % adequação da matriz nos poços produtores
 % if max(wells)~=0
