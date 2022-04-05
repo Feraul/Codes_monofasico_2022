132a5a7d69d0daf002e682009e906904904756a9
Fernando Contreras <ferlicapac@gmail.com>
2022-04-05 19:25:38 -0300

    colocamos a matriz de rotacion

--- gravitation.m
+++ gravitation.m
@@ -3,17 +3,19 @@ global inedge bedge elem centelem coord normals
 Klef=zeros(2,2);
 Krel=zeros(2,2);
 G=zeros(size(elem,1),1);
+R=[0 1 ;-1 0 ];
 for i=1:size(bedge,1)
 
-    lef=bedge(i,3); 
+    lef=bedge(i,3);
+    ve1=coord(bedge(i,2),1:2)-coord(bedge(i,1),1:2);
     if bedge(i,5)<200
-         
+        
         % tensor de permeabilidade do elemento a esquerda
         Klef(1,1)=kmap(elem(lef,5),2);
         Klef(1,2)=kmap(elem(lef,5),3);
         Klef(2,1)=kmap(elem(lef,5),4);
         Klef(2,2)=kmap(elem(lef,5),5);
-        g(i,1)=dot(normals(i,1:2),(Klef*grav(lef,:)')');
+        g(i,1)=dot((R*ve1')',(Klef*grav(lef,:)')');
         
         G(lef,1)=G(lef,1)+g(i,1);
     else
@@ -28,6 +30,7 @@ for ii=1:size(inedge,1)
         
         lef=inedge(ii,3);
         rel=inedge(ii,4);
+        ve2=(coord(inedge(ii,2),1:2)-coord(inedge(ii,1),1:2));
         % calculo do ponto meio da face        
         vm=(coord(inedge(ii,1),1:2)+coord(inedge(ii,2),1:2))*0.5;
         % calculo da distancia do centro ao ponto meio da face 
@@ -50,7 +53,7 @@ for ii=1:size(inedge,1)
         
         Keq=inv(dj1*inv(Klef)+dj2*inv(Krel)); % equation 21
         graveq=(dj1*grav(lef,:)+dj2*grav(rel,:))'; % equation 22
-        g(ii+size(bedge,1),1)=dot(normals(ii+size(bedge,1),1:2),(Keq*graveq)'); % equation 20
+        g(ii+size(bedge,1),1)=dot((R*ve2')',(Keq*graveq)'); % equation 20
         
         G(lef,1)=G(lef,1)+g(ii+size(bedge,1),1);
         G(rel,1)=G(rel,1)-g(ii+size(bedge,1),1);
