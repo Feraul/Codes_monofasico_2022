8d5f495f488e727cf806a44c5674688d15a91fbd
Fernando Contreras <ferlicapac@gmail.com>
2022-04-06 20:10:42 -0300

    voltamos a incluir os normals

--- gravitation.m
+++ gravitation.m
@@ -4,37 +4,31 @@ Klef=zeros(2,2);
 Krel=zeros(2,2);
 G=zeros(size(elem,1),1);
 R=[0 1 ;-1 0 ];
-for i=1:size(bedge,1)
-
-    lef=bedge(i,3);
-    ve1=coord(bedge(i,2),1:2)-coord(bedge(i,1),1:2);
-    if bedge(i,5)<200
+for i=1:size(inedge,1)+size(bedge,1)
+    
+    if i<size(bedge,1) || i==size(bedge,1)
         
+        lef=bedge(i,3);
         % tensor de permeabilidade do elemento a esquerda
         Klef(1,1)=kmap(elem(lef,5),2);
         Klef(1,2)=kmap(elem(lef,5),3);
         Klef(2,1)=kmap(elem(lef,5),4);
         Klef(2,2)=kmap(elem(lef,5),5);
-        g(i,1)=dot((R*ve1')',(Klef*grav(lef,:)')');
+        g(i,1)=dot(normals(i,1:2),(Klef*grav(lef,:)')');
         
         G(lef,1)=G(lef,1)+g(i,1);
     else
-       g(i,1)=0;
         
-        G(lef,1)=G(lef,1)+g(i,1); 
-    end
-end
-
-for ii=1:size(inedge,1)
-    
+        
+        ii=i-size(bedge,1);
         
         lef=inedge(ii,3);
         rel=inedge(ii,4);
-        ve2=(coord(inedge(ii,2),1:2)-coord(inedge(ii,1),1:2));
-        % calculo do ponto meio da face        
+        
+        % calculo do ponto meio da face
         vm=(coord(inedge(ii,1),1:2)+coord(inedge(ii,2),1:2))*0.5;
-        % calculo da distancia do centro ao ponto meio da face 
-        dj1=norm(centelem(lef,1:2)-vm); 
+        % calculo da distancia do centro ao ponto meio da face
+        dj1=norm(centelem(lef,1:2)-vm);
         dj2=norm(centelem(rel,1:2)-vm);
         
         % tensor do elemento a esquerda
@@ -53,11 +47,12 @@ for ii=1:size(inedge,1)
         
         Keq=inv(dj1*inv(Klef)+dj2*inv(Krel)); % equation 21
         graveq=(dj1*grav(lef,:)+dj2*grav(rel,:))'; % equation 22
-        g(ii+size(bedge,1),1)=dot((R*ve2')',(Keq*graveq)'); % equation 20
+        g(i,1)=dot(normals(i,1:2),(Keq*graveq)'); % equation 20
         
         G(lef,1)=G(lef,1)+g(ii+size(bedge,1),1);
         G(rel,1)=G(rel,1)-g(ii+size(bedge,1),1);
+    end
+    
+    
 end
-
-
 end
