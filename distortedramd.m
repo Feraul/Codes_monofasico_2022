b5867ffc725f2acfd05c95b4094412c2428a663d
Fernando Contreras <ferlicapac@gmail.com>
2022-04-13 07:33:51 -0300

    mudanca do h

--- distortedramd.m
+++ distortedramd.m
@@ -9,16 +9,16 @@ b=1;
 % com alpha 0.8 e h=1/220
 ex = a + (b-a)*rand(size(coord,1),1);
 ey = a + (b-a)*rand(size(coord,1),1);
-% qquando mas proximo a 1 eh mas ditorcido
-alpha=0.25;
-h=1/128;
+% quando mas proximo a 1 eh mas ditorcido
+alpha=0.29;
+h=1/16;
 
 for icoord=1:size(coord,1)
     x=coord(icoord,1);
     
     y=coord(icoord,2);
     if icoord>size(bedge,1) %&& abs(coord(icoord,1)-0.5)>1e-10
-        
+       if single(y)~=0.5       
 %        if x~=0.5 && y~=0.5
             x=coord(icoord,1)+h*alpha*ex(icoord,1);
             
@@ -27,11 +27,11 @@ for icoord=1:size(coord,1)
             auxcoord(icoord,1)=icoord;
             auxcoord(icoord,2)=x;
             auxcoord(icoord,3)=y;
-%         else
-%             auxcoord(icoord,1)=icoord;
-%             auxcoord(icoord,2)=x;
-%             auxcoord(icoord,3)=y;
-%        end
+         else
+             auxcoord(icoord,1)=icoord;
+             auxcoord(icoord,2)=x;
+             auxcoord(icoord,3)=y;
+        end
     else
 
         auxcoord(icoord,1)=icoord;
