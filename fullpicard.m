96a20703473fdc9949ea55e2f8031d1e575f3b68
Fernando Contreras <ferlicapac@gmail.com>
2022-04-13 07:31:24 -0300

    argumentos de entrada

--- fullpicard.m
+++ fullpicard.m
@@ -1,5 +1,5 @@
 function [p_new,tabletol,step,ciclos]=fullpicard(M_old,RHS_old,nitpicard,tolpicard,kmap,...
-    parameter,metodoP,w,s,nflagface,fonte,p_old,gamma,nflagno,benchmark,...
+    parameter,w,s,nflagface,fonte,p_old,gamma,nflagno,...
     weightDMP,auxface,wells,mobility,Hesq, Kde, Kn, Kt, Ded,calnormface,gravresult)
 ciclos=1;
 % calculo do residuo Inicial
@@ -22,11 +22,11 @@ while (tolpicard<er || tolpicard==er) && (step<nitpicard)
     %         p_max=max(p_new)
     %         p_min=min(p_new)
     % Interpolação das pressões na arestas (faces)
-    [pinterp_new]=pressureinterp(p_new,nflagface,nflagno,w,s,metodoP,parameter,weightDMP);
+    [pinterp_new]=pressureinterp(p_new,nflagface,nflagno,w,s,parameter,weightDMP);
     
     % Calculo da matriz global
     [M_new,RHS_new]=globalmatrix(p_new,pinterp_new,gamma,nflagface,nflagno...
-        ,parameter,kmap,fonte,metodoP,w,s,benchmark,weightDMP,auxface,...
+        ,parameter,kmap,fonte,w,s,weightDMP,auxface,...
         wells,mobility,Hesq, Kde, Kn, Kt, Ded,calnormface,gravresult);
     min(RHS_new)
     % calculo do residuo
