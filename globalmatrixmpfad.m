d61e2178b8f94caad5f6aef6162465531c557ece
Fernando Contreras <ferlicapac@gmail.com>
2022-04-14 18:57:03 -0300

    estrategia starnoni ou inhouse

--- globalmatrixmpfad.m
+++ globalmatrixmpfad.m
@@ -1,7 +1,8 @@
 function [ M, I ] = globalmatrixmpfad( w,s, Kde, Ded, Kn, Kt, nflagno, ...
     Hesq,fonte,gravresult,gravrate,gravno,gravelem)
 
-global coord elem esurn1 esurn2  bedge inedge  centelem bcflag gravitational
+global coord elem esurn1 esurn2  bedge inedge  centelem bcflag gravitational...
+    strategy
 
 %-----------------------inicio da rutina ----------------------------------%
 %Constrói a matriz global.
@@ -22,65 +23,67 @@ for ifacont=1:size(bedge,1)
     normcont=norm(v0);
     
     % Tratamento do nó nos vértices 2 e 4%
-      A=-Kn(ifacont)/(Hesq(ifacont)*norm(v0));  
+    A=-Kn(ifacont)/(Hesq(ifacont)*norm(v0));
     if bedge(ifacont,5)<200
         c1=nflagno(bedge(ifacont,1),2);
         c2=nflagno(bedge(ifacont,2),2);
-       
-        %Preenchimento
+        
+        %Preenchimento do termo gravitacional
         
         if strcmp(gravitational,'yes')
-            m1=gravno(bedge(ifacont,1),1);
-            m2=gravno(bedge(ifacont,2),1);
-            m=A*(dot(v2,-v0)*m1+dot(v1,v0)*m2-norm(v0)^2*gravelem(lef))-(m2-m1)*Kt(ifacont);
-           %m=gravrate(ifacont);
+            if strcmp(strategy,'starnoni')
+                m=gravrate(ifacont);
+            elseif strcmp(strategy,'inhouse')
+                g1=gravno(bedge(ifacont,1),1); % gravidade no vertice 1
+                g2=gravno(bedge(ifacont,2),1); % gravidade no vertice 2
+                m=A*(dot(v2,-v0)*g1+dot(v1,v0)*g2-norm(v0)^2*gravelem(lef))-(g2-g1)*Kt(ifacont);
+            end
         else
             m=0;
         end
         
-        M(bedge(ifacont,3),bedge(ifacont,3))=M(bedge(ifacont,3),bedge(ifacont,3))-A*(norm(v0)^2);
+        M(lef,lef)=M(lef,lef)-A*(norm(v0)^2);
         
-        I(bedge(ifacont,3))=I(bedge(ifacont,3))-A*(dot(v2,-v0)*c1+dot ...
-            (v1,v0)*c2)+(c2-c1)*Kt(ifacont)-m;
+        I(lef)=I(lef)-A*(dot(v2,-v0)*c1+dot(v1,v0)*c2)+(c2-c1)*Kt(ifacont)-m;
         
     else
-       
-%                 if strcmp(gravitational,'yes')
-%                     no1=bedge(ifacont,1);
-%                     no2=bedge(ifacont,2);
-%                     nec1=esurn2(no1+1)-esurn2(no1);
-%                     nec2=esurn2(no2+1)-esurn2(no2);
-%                     g1=0;
-%                     if nflagno(no1,1)<200
-%                         g1=gravno(no1,1);
-%                     else
-%                         for j=1:nec1
-%                             element1=esurn1(esurn2(no1)+j);
-%                             g1=g1+w(esurn2(no1)+j)*gravelem(element1);
-%                         end
-%                     end
-%                     g2=0;
-%                     if  nflagno(no2,1)<200
-%                         g2=gravno(no2,1);
-%                     else
-%                         for j=1:nec2
-%                             element2=esurn1(esurn2(no2)+j);
-%                             g2=g2+w(esurn2(no2)+j)*gravelem(element2);
-%                         end
-%                     end
-%         
-%                     m=A*(dot(v2,-v0)*g1+dot(v1,v0)*g2-norm(v0)^2*gravelem(lef))-(g2-g1)*Kt(ifacont);
-%                     % m=gravrate(ifacont);
-%                 else
-%                     m=0;
-%                 end
-       
+        
+        %                 if strcmp(gravitational,'yes')
+        %                     no1=bedge(ifacont,1);
+        %                     no2=bedge(ifacont,2);
+        %                     nec1=esurn2(no1+1)-esurn2(no1);
+        %                     nec2=esurn2(no2+1)-esurn2(no2);
+        %                     g1=0;
+        %                     if nflagno(no1,1)<200
+        %                         g1=gravno(no1,1);
+        %                     else
+        %                         for j=1:nec1
+        %                             element1=esurn1(esurn2(no1)+j);
+        %                             g1=g1+w(esurn2(no1)+j)*gravelem(element1);
+        %                         end
+        %                     end
+        %                     g2=0;
+        %                     if  nflagno(no2,1)<200
+        %                         g2=gravno(no2,1);
+        %                     else
+        %                         for j=1:nec2
+        %                             element2=esurn1(esurn2(no2)+j);
+        %                             g2=g2+w(esurn2(no2)+j)*gravelem(element2);
+        %                         end
+        %                     end
+        %
+        %                     m=A*(dot(v2,-v0)*g1+dot(v1,v0)*g2-norm(v0)^2*gravelem(lef))-(g2-g1)*Kt(ifacont);
+        %                     % m=gravrate(ifacont);
+        %                 else
+        %                     m=0;
+        %                 end
+        
         % contorno de Neumann
         x=bcflag(:,1)==bedge(ifacont,5);
         r=find(x==1);
-        I(bedge(ifacont,3))=I(bedge(ifacont,3)) -normcont*bcflag(r,2);%+m;
+        I(lef)=I(lef) -normcont*bcflag(r,2);%+m;
     end
-   
+    
 end
 
 
@@ -91,21 +94,21 @@ for iface=1:size(inedge,1)
     %Contabiliza as contribuições do fluxo numa aresta para os elementos %
     %a direita e a esquerda dela.                                        %
     
-    M(inedge(iface,3), inedge(iface,3))=M(inedge(iface,3), inedge(iface,3))- Kde(iface);
-    M(inedge(iface,3), inedge(iface,4))=M(inedge(iface,3), inedge(iface,4))+ Kde(iface);
-    M(inedge(iface,4), inedge(iface,4))=M(inedge(iface,4), inedge(iface,4))- Kde(iface);
-    M(inedge(iface,4), inedge(iface,3))=M(inedge(iface,4), inedge(iface,3))+ Kde(iface);
+    M(lef, lef)=M(lef, lef)- Kde(iface);
+    M(lef, rel)=M(lef, rel)+ Kde(iface);
+    M(rel, rel)=M(rel, rel)- Kde(iface);
+    M(rel, lef)=M(rel, lef)+ Kde(iface);
     
     %Se os nós das arestas estiverem em fronteiras de Dirichlet, suas
     %contribuições serão contabilizadas logo abaixo.
     
     if nflagno(inedge(iface,1),1)<200
-        I(inedge(iface,3))=I(inedge(iface,3))-Kde(iface)*Ded(iface)*nflagno(inedge(iface,1),2);
-        I(inedge(iface,4))=I(inedge(iface,4))+Kde(iface)*Ded(iface)*nflagno(inedge(iface,1),2);
+        I(lef)=I(lef)-Kde(iface)*Ded(iface)*nflagno(inedge(iface,1),2);
+        I(rel)=I(rel)+Kde(iface)*Ded(iface)*nflagno(inedge(iface,1),2);
     end
     if nflagno(inedge(iface,2),1)<200
-        I(inedge(iface,3))=I(inedge(iface,3))+Kde(iface)*Ded(iface)*nflagno(inedge(iface,2),2);
-        I(inedge(iface,4))=I(inedge(iface,4))-Kde(iface)*Ded(iface)*nflagno(inedge(iface,2),2);
+        I(lef)=I(lef)+Kde(iface)*Ded(iface)*nflagno(inedge(iface,2),2);
+        I(rel)=I(rel)-Kde(iface)*Ded(iface)*nflagno(inedge(iface,2),2);
     end
     % quando o nó pertece ao contorno de Neumann
     if nflagno(inedge(iface,1),1)==202
@@ -130,9 +133,9 @@ for iface=1:size(inedge,1)
             
             post_cont=esurn2(inedge(iface,1))+j;
             
-            M(inedge(iface,3), esurn1(post_cont))=M(inedge(iface,3),esurn1(post_cont)) + Kde(iface)*Ded(iface)*w(post_cont);
+            M(lef, esurn1(post_cont))=M(lef,esurn1(post_cont)) + Kde(iface)*Ded(iface)*w(post_cont);
             
-            M(inedge(iface,4), esurn1(post_cont))=M(inedge(iface,4),esurn1(post_cont)) - Kde(iface)*Ded(iface)*w(post_cont);
+            M(rel, esurn1(post_cont))=M(rel,esurn1(post_cont)) - Kde(iface)*Ded(iface)*w(post_cont);
             
         end
     end
@@ -141,40 +144,47 @@ for iface=1:size(inedge,1)
             
             post_cont=esurn2(inedge(iface,2))+j;
             
-            M(inedge(iface,3), esurn1(post_cont))=M(inedge(iface,3),esurn1(post_cont)) - Kde(iface)*Ded(iface)*w(post_cont);
+            M(lef, esurn1(post_cont))=M(lef,esurn1(post_cont)) - Kde(iface)*Ded(iface)*w(post_cont);
             
-            M(inedge(iface,4), esurn1(post_cont))=M(inedge(iface,4),esurn1(post_cont)) + Kde(iface)*Ded(iface)*w(post_cont);
+            M(rel, esurn1(post_cont))=M(rel,esurn1(post_cont)) + Kde(iface)*Ded(iface)*w(post_cont);
+            
+            % I(esurn1(post_cont))=I(esurn1(post_cont))-Kde(iface)*Ded(iface)*w(post_cont)*gravelem(esurn1(post_cont));
         end
     end
+    % termo gravitacional
     if strcmp(gravitational,'yes')
-        no1=inedge(iface,1);
-        no2=inedge(iface,2);
-        nec1=esurn2(no1+1)-esurn2(no1);
-        nec2=esurn2(no2+1)-esurn2(no2);
-        g1=0;
-        if nflagno(no1,1)<200
-         g1=gravno(no1,1);   
-        else
-            for j=1:nec1
-                element1=esurn1(esurn2(no1)+j);
-                g1=g1+w(esurn2(no1)+j)*gravelem(element1);
+        
+        if strcmp(strategy,'starnoni')
+            m=gravrate(size(bedge,1)+iface,1);
+        elseif strcmp(strategy,'inhouse')
+            no1=inedge(iface,1);
+            no2=inedge(iface,2);
+            nec1=esurn2(no1+1)-esurn2(no1);
+            nec2=esurn2(no2+1)-esurn2(no2);
+            g1=0;
+            if nflagno(no1,1)<200
+                g1=gravno(no1,1);
+            else
+                for j=1:nec1
+                    element1=esurn1(esurn2(no1)+j);
+                    g1=g1+w(esurn2(no1)+j)*gravelem(element1);
+                end
             end
-        end
-        g2=0;
-        if nflagno(no2,1)<200
-          g2=gravno(no2,1);  
-        else
-            for jj=1:nec2
-                element2=esurn1(esurn2(no2)+jj);
-                g2=g2+w(esurn2(no2)+jj)*gravelem(element2);
+            g2=0;
+            if nflagno(no2,1)<200
+                g2=gravno(no2,1);
+            else
+                for jj=1:nec2
+                    element2=esurn1(esurn2(no2)+jj);
+                    g2=g2+w(esurn2(no2)+jj)*gravelem(element2);
+                end
             end
+            
+            m= Kde(iface)*(gravelem(rel,1)-gravelem(lef,1)-Ded(iface)*(g2-g1));
         end
         
-        m= Kde(iface)*(gravelem(rel,1)-gravelem(lef,1)-Ded(iface)*(g2-g1));
-        
-        %m=gravrate(size(bedge,1)+iface,1);
-        I(inedge(iface,3))=I(inedge(iface,3))-m;
-        I(inedge(iface,4))=I(inedge(iface,4))+m;
+        I(lef)=I(lef)-m;
+        I(rel)=I(rel)+m;
         
     end
 end
