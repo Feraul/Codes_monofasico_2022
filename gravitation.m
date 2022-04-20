a3b5afe6b3dddefe78cd54e305a3c77086dbbc64
Fernando Contreras <ferlicapac@gmail.com>
2022-04-20 19:24:21 -0300

    modificado

--- gravitation.m
+++ gravitation.m
@@ -4,10 +4,12 @@ Klef=zeros(2,2);
 Krel=zeros(2,2);
 G=zeros(size(elem,1),1);
 R=[0 1 ;-1 0 ];
+
 for i=1:size(inedge,1)+size(bedge,1)
     
     if i<size(bedge,1) || i==size(bedge,1)
         
+        ve1=coord(bedge(i,2),1:2)-coord(bedge(i,1),1:2);
         lef=bedge(i,3);
         % tensor de permeabilidade do elemento a esquerda
         Klef(1,1)=kmap(elem(lef,5),2);
@@ -16,10 +18,13 @@ for i=1:size(inedge,1)+size(bedge,1)
         Klef(2,2)=kmap(elem(lef,5),5);
         x=(coord(bedge(i,1),1)+coord(bedge(i,2),1))*0.5;
         y=(coord(bedge(i,1),2)+coord(bedge(i,2),2))*0.5;
-        gravface1=[cosd(x)*cosd(y) -sind(x)*sind(y)];
-        %g(i,1)=dot(normals(i,1:2),(Klef*grav(lef,1:2)')');
-        g(i,1)=dot(normals(i,1:2),(Klef*gravface1')');
-       % g(i,1)=gravface(i,1);
+        gravface1=[cos(x)*cos(y) -sin(x)*sin(y)];
+        
+        %gravface1=[0 sind(x)*cosd(y)/y];
+        g(i,1)=dot((R*ve1')',(Klef*grav(lef,1:2)')');
+        %g(i,1)=dot(normals(i,1:2),(Klef*gravface1')');
+         
+        %g(i,1)=gravface(i,1);
         G(lef,1)=G(lef,1)+g(i,1);
     else
         
@@ -31,6 +36,7 @@ for i=1:size(inedge,1)+size(bedge,1)
         
         % calculo do ponto meio da face
         vm=(coord(inedge(ii,1),1:2)+coord(inedge(ii,2),1:2))*0.5;
+        vd1=coord(inedge(ii,2),1:2)-coord(inedge(ii,1),1:2);
         % calculo da distancia do centro ao ponto meio da face
         dj1=norm(centelem(lef,1:2)-vm);
         dj2=norm(centelem(rel,1:2)-vm);
@@ -51,7 +57,7 @@ for i=1:size(inedge,1)+size(bedge,1)
         
         Keq=inv(dj1*inv(Klef)+dj2*inv(Krel)); % equation 21
         graveq=(dj1*grav(lef,:)+dj2*grav(rel,:))'; % equation 22
-        g(i,1)=dot(normals(i,1:2),(Keq*graveq)'); % equation 20
+        g(i,1)=dot((R*vd1')',(Keq*graveq)'); % equation 20
         
         G(lef,1)=G(lef,1)+g(i,1);
         G(rel,1)=G(rel,1)-g(i,1);
